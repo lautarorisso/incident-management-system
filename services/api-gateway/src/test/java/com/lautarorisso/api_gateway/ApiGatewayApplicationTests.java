9eb3d82ad5caa2eacb01e7702e093fcc4822c137
lautaro <lautarorisso77@gmail.com>
2026-08-02 00:08:50 -0300

    chore: minor config and test formatting fixes
    - discovery-service: restructure prometheus metrics export config
    - api-gateway: add test config to disable eureka/config in ApiGatewayApplicationTests

--- services/api-gateway/src/test/java/com/lautarorisso/api_gateway/ApiGatewayApplicationTests.java
+++ services/api-gateway/src/test/java/com/lautarorisso/api_gateway/ApiGatewayApplicationTests.java
@@ -2,8 +2,13 @@ package com.lautarorisso.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "eureka.client.enabled=false",
+        "spring.cloud.config.enabled=false"
+})
+@Import(TestSecurityConfig.class)
 class ApiGatewayApplicationTests {
 
 	@Test
